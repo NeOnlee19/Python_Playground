d25f0cee0a16982fad5ae5b2a64e08c3db8ad94f
Aleksandra Stanisz <olastanisz@gmail.com>
2026-01-04 15:00:23 +0100

    Added password encrypting

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,13 +1,34 @@
+from cryptography.fernet import Fernet
+
+def load_key():
+    f = open("key.key", "rb")
+    key = f.read()
+    f.close()
+    return
+
 master_pwd = input("Enter master password: ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key) '''
+
 def view():
-    pass
+    with open("password.txt", "r") as file:
+        for line in file.readlines():
+            data = line.rstrip()
+            user, password = data.split("|")
+            print("User:", user, "Password:", password)
 
 def add():
     name = input("Account name: ")
     pwd = input("Password: ")
 
     with open("password.txt", "a") as file:
-        file.write(name + "|" + pwd)
+        file.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones? (add/view/quit): ").lower()
