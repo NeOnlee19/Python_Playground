3aaac32f4d5e46db4f08d01224f1f595dbdbd8bd
Aleksandra Stanisz <olastanisz@gmail.com>
2026-01-08 14:12:45 +0100

    Added deposit function and validation

---
+++ Slot_Machine/main.py
@@ -0,0 +1,13 @@
+
+def deposit():
+    while True:
+        amount = input("How much do you want to deposit: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter a positive number")
+        else:
+            print("Please enter a number")
+    return amount
