a4981cfab8d258f215bcd4e972199b4288051af4
Aleksandra Stanisz <olastanisz@gmail.com>
2026-01-12 20:28:54 +0100

    Added balance and game logic

--- Slot_Machine/main.py
+++ Slot_Machine/main.py
@@ -103,8 +103,7 @@ def get_bet():
             print("Please enter a number")
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -122,5 +121,17 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}")
     print(f"You won on lines:", *winning_lines)
+
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(F"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    print(f"You left with ${balance}")
 
 main()
